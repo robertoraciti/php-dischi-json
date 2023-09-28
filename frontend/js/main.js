a5759b7293ea4ee4d3f68f8623ae99c4f77cba2e
robertoraciti <rob.raciti97@gmail.com>
2023-09-28 16:48:16 +0200

    complete ex without bonus

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,7 +3,16 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      disks: [],
     };
+  },
+
+  mounted() {
+    axios
+      .get("http://localhost:8888/php-dischi-json/backend/data/dischi.json")
+      .then((response) => {
+        this.disks = response.data;
+        console.log(this.disks);
+      });
   },
 }).mount("#app");
